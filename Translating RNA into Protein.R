d7f31708d0c18e83b290397a197577e003f3838a
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 14:10:09 -0600

    Update Translating RNA into Protein.R

--- Translating RNA into Protein.R
+++ Translating RNA into Protein.R
@@ -1,5 +1,10 @@
+# Translating RNA into Protein (Rosalind PROT) - Biostrings Version
+# Author: Kien Nguyen
+# Uses Biostrings::translate for efficient, production-ready translation
+
 library(Biostrings)
 sequence <- readRNAStringSet("rosalind_prot (1).txt")
+class(sequence)
 translated_seq <- function(sequence){
   #Translate from RNA to protein sequence
   prot <- translate(sequence, no.init.codon =T)

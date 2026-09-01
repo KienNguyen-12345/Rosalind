167bcf539cdd850da36493a0e2a75d9525971e65
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:03:31 -0600

    Update Complementing a Strand of DNA.R

--- Complementing a Strand of DNA.R
+++ Complementing a Strand of DNA.R
@@ -1,11 +1,13 @@
 # Complementing a Strand of DNA (Rosalind REVC) - Biostrings Version
 # Author: Kien Nguyen
 # Uses Biostrings::reverseComplement for production efficiency
-
+#Load package
 library(Biostrings)
 
+#Input data
 seq <- readLines("rosalind_revc.txt")
 
+#Create a pipeline sequence in which the data from sequence goes through DNAString() to reverseComplement to come back to individual string then print
 seq %>%
   DNAString() %>%
   reverseComplement() %>%

c7cd0083d0800fa60216bdb87a1f37320332a15c
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 11:50:02 -0600

    Add files via upload

---
+++ Counting DNA Nucleotides_ver 2.R
@@ -0,0 +1,14 @@
+# Counting DNA Nucleotides (Rosalind DNA)
+# Author: Kien Nguyen
+# Reads rosalind_dna.txt and prints A C G T counts
+
+data <- readLines("rosalind_dna.txt")
+
+bases <- strsplit(data, "")[[1]]
+bases
+a_count <- sum(bases %in% "A")
+g_count<- sum(bases %in% "G")
+c_count<- sum(bases %in% "C")
+t_count<- sum(bases %in% "T")
+
+cat(a_count, c_count, g_count, t_count)

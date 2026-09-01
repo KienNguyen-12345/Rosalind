83ea86caabc461b2436b2c9e085ad270955df0e3
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:33:05 -0600

    Update Computing GC Content_ver3.R

--- Computing GC Content_ver3.R
+++ Computing GC Content_ver3.R
@@ -1,11 +1,16 @@
 # Computing GC Content (Rosalind GC) - Biostrings Optimized
 # Author: Kien Nguyen
 # Uses Biostrings::letterFrequency in one vectorized operation
-
+#Load package:
 library(Biostrings)
+#Input data:
 sequences <- readDNAStringSet("rosalind_gc.txt")
+#Since the rosalind_gc.txt file contains more than 1 sequences,letterFrequency command will return as a matrix
+# That's why we will use rowSums to count all the incidences of "G" and "C" nucleotides appear.
 gc_count <- rowSums(letterFrequency(sequences, letters = c("G","C")))
+#Get the FASTA title of each sequence and put them into "ids" variable
 ids <- names(sequences)
+#Caculate the gc percent
 gc_percent <- gc_count/width(sequences)*100
 
 #Find max:

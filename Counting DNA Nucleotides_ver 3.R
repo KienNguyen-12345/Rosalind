b155479cb303ca04251edd5805f0beeb1ba5c31d
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:56:15 -0600

    Update Counting DNA Nucleotides_ver 3.R

--- Counting DNA Nucleotides_ver 3.R
+++ Counting DNA Nucleotides_ver 3.R
@@ -2,8 +2,11 @@
 # Author: Kien Nguyen
 # Reads rosalind_dna.txt and prints A C G T counts
 
+#Input data:
 data <- readLines("rosalind_dna.txt")
 
+#strsplit separate the sequence into a list of individual character
+#unlist combine all of these individual characters into a vector containing all of the characters
 bases <- unlist(strsplit(data, ""))
 bases
 a_count <- sum(bases %in% "A")

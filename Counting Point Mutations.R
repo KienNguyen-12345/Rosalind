1425464c2016fd4fee09a43f3c5bc7d5b3b9d4f4
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 12:46:57 -0600

    Refactor to vectorized Hamming distance calculation
    Refactored code to use a function for calculating Hamming distance and improved vectorization.

--- Counting Point Mutations.R
+++ Counting Point Mutations.R
@@ -2,17 +2,13 @@
 # Author: Kien Nguyen
 # Uses vectorized comparison to calculate Hamming distance efficiently
 
-sequences <- readLines("rosalind_hamm.txt")
-s1 <- strsplit(sequences[[1]], "")[[1]]
-s2 <- strsplit(sequences[[2]], "")[[1]]
-
-mutations <- 0
-for(i in seq_along(s1)){
-  b1 <- s1[i]
-  b2 <- s2[i]
-
-if(b1 != b2){
-  mutations <- mutations +1
+result <- function(s1, s2) {
+  s1_vec <- unlist(strsplit(s1, ""))
+  s2_vec <- unlist(strsplit(s2, ""))
+  missmatch <- (s1_vec != s2_vec)
+  cat(sum(missmatch))
 }
-}
-cat(mutations)
+
+s1 <- readLines("rosalind_hamm.txt")[1]
+s2 <- readLines("rosalind_hamm.txt")[2]
+result(s1, s2)

bfa6d3dc04640e2e4d00ca33480c543c95665881
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:33:51 -0600

    Update Counting DNA Nucleotides.R

--- Counting DNA Nucleotides.R
+++ Counting DNA Nucleotides.R
@@ -1,9 +1,12 @@
 # Counting DNA Nucleotides (Rosalind DNA)
 # Author: Kien Nguyen
 # Reads rosalind_dna.txt and prints A C G T counts
+#Load package:
 library(stringr)
 
+#read data:
 data <- readLines("rosalind_dna.txt")
 
+#count each of A, C, G, and T nucleotides:
 count_2 <- str_count(data,pattern = c("A","C","G","T"))
 cat(count_2)

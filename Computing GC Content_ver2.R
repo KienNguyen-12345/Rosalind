399957250e85c03e0765cc5c43b968378c7ef45c
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:58:41 -0600

    Update Computing GC Content_ver2.R

--- Computing GC Content_ver2.R
+++ Computing GC Content_ver2.R
@@ -2,15 +2,22 @@
 # Author: Kien Nguyen
 # No Biostrings manipulation - uses strsplit and sapply
 # To the underlying algorithm
-
+#Load package
 library(Biostrings)
+#input data
 sequences <- readDNAStringSet("rosalind_gc.txt")
+#Change the class of object from DNA to character
 sequences <- as.character(sequences)
+#Take the headname of each sequence and make them becomes ids
 ids <- names(sequences)
 
+#make function:
 gc_percent <- sapply(sequences, function(seq){
+  #split the string into individuals characters then combine them into a vector
   bases <- strsplit(seq,"")[[1]]
+  #Count all of the incidence of G and C appear
   gc_count <- sum(bases %in% c("G","C"))
+  #Calculate gc percent
   gc_count/length(bases)*100
 })
 max_idx <- which.max(gc_percent)

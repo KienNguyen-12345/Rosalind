14a53137e14b0484c8f9186d59e8a76fb45f087c
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-01 13:23:42 -0600

    Update Complementing a Strand of DNA_ver2.R

--- Complementing a Strand of DNA_ver2.R
+++ Complementing a Strand of DNA_ver2.R
@@ -3,10 +3,16 @@
 # No external dependencies - manual reverse complement
 # Step 1: Complement (A<->T, C<->G)
 # Step 2: Reverse the string
-
+#Input data:
 seq <- readLines("rosalind_revc.txt")
 
+#Dynamic character replacement: A -> T, C -> G, T -> A, G -> C
 comp_seq <- chartr("ACTG", "TGAC", seq)
-reverse_string <- paste(rev(unlist(strsplit(comp_seq, NULL))), collapse = "")
-
+#Reverse the sequence of comp_seq to make it reverse from the seq sequence
+reverse_string <- paste(rev(unlist(strsplit(comp_seq, split = ""))), collapse = "")
+#explain:
+# strsplit(comp_seq,split = "") means split the string into a list using no space as pattern
+# unlist(strsplit()) is used to make the list just created to individual vectors
+# rev() reverse the sequence of vectors created
+# Paste() Concatenate vectors after converting to character
 cat(reverse_string)

cd5fa91f8a22224d51338796ce90d3e904824106
KienNguyen-12345 <kienkent2004@gmail.com>
2026-09-02 15:47:20 -0600

    Update Finding a Shared Motif.R

--- Finding a Shared Motif.R
+++ Finding a Shared Motif.R
@@ -1,16 +1,26 @@
 library(Biostrings)
-bases <- c("A","C","G","T")
-
-xx <- DNAMultipleAlignment(readDNAStringSet("rosalind_cons.txt"))
-
-result <- function(xx){
-profile <-consensusMatrix(xx, bases,
-                          as.prob = FALSE) [1:4,]
-concensus <- paste0(bases[apply(profile, 2, which.max)],
-                    collapse = "")
-cat(concensus)
+sequences <- as.character(readDNAStringSet("rosalind_lcsm.txt"))
+find_shared_motif <- function(sequences){
+  #identify the shortest sequence (as a plain string)
+  shortest <- sequences[which.min(nchar(sequences))]
+  #start with full length of shortest sequence
+  L <- nchar(shortest)
+  for(len in L:1){
+    #Identify least possible starting position
+    max_start <- L - len + 1
+    #All possible starting position:
+    start_indices <- 1: max_start
+    # Extract all possible substrings:
+    substrings <- sapply(start_indices,
+                         function(i)substr(shortest, i, i+len-1))
+    #Check each substring:
+    for(motif in substrings){
+      # Test if motif appears in every sequence (using fixed = TRUE for exact match)
+      if (all(sapply(sequences, 
+                     function(s) grepl(motif, s, fixed = TRUE))))
+        return(motif, file= "result.txt")
+    }
+  }
 }
-result(xx)
-result
-concensus
-profile
+
+find_shared_motif(sequences)
